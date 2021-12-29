0e4801ccdec142a342a3a9fed1813d6a5fba9c7c
alpheratz0 <alpheratz99@protonmail.com>
2021-12-29 04:41:22 -0300

    fix: change license header to gpl version 2 only

--- gameoflife.c
+++ gameoflife.c
@@ -2,8 +2,8 @@
 	Copyright (C) 2021 <alpheratz99@protonmail.com>
 
 	This program is free software; you can redistribute it and/or modify it under 
-	the terms of the GNU General Public License as published by the Free Software 
-	Foundation; either version 2 of the License, any later version.
+	the terms of the GNU General Public License version 2 as published by the 
+	Free Software Foundation.
 
 	This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
